e9e8aab5f615a9db9b51de9da98a0e5237a4d148
unknown <aaron.j.blancaflor@gmail.com>
2017-09-10 22:13:54 -0700

    simple request call and accessing the lat, long, and formatted_address

--- project3-weatherApp/app.js
+++ project3-weatherApp/app.js
@@ -1,13 +1,16 @@
 const request = require('request');
 
 //making a request
+// accepts an object, and a callback function.
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=950%20Glenn%20Dr%20folsom',
     json: true
 }, (error, response, body)=>{
     // inside callback function for the request.
 
-    // JSON.stringify makes it easy to see the json object. Takes 3 arguments,
-    //value, replacer, and number of spaces.
-    console.log(JSON.stringify(body, undefined, 4));
+    //accessomg formatted address, lat, and lng
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+
 });
