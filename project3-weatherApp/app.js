4f09c3cb0705230c6b092ef51575ef18f0f2ea62
unknown <aaron.j.blancaflor@gmail.com>
2017-09-10 21:20:44 -0700

    changed addy

--- project3-weatherApp/app.js
+++ project3-weatherApp/app.js
@@ -2,8 +2,12 @@ const request = require('request');
 
 //making a request
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1122%20Skywest%20Ct%20Fairfield',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=950%20Glenn%20Dr%20folsom',
     json: true
 }, (error, response, body)=>{
-    console.log(body);
+    // inside callback function for the request.
+
+    // JSON.stringify makes it easy to see the json object. Takes 3 arguments,
+    //value, replacer, and number of spaces.
+    console.log(JSON.stringify(body, undefined, 4));
 });
